ba5a623b8e1c4b95f4ac52ec80b289c1e4f98018
Tiffany Gu <tiffanygu@ucsb.edu>
2025-04-30 18:28:58 -0700

    tg-updated format for menuItemReviewFixtures

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -1,5 +1,14 @@
 const menuItemReviewFixtures = {
-    oneReview: {
+  oneReview: {
+    id: 1,
+    itemId: "10",
+    reviewerEmail: "cgaucho@ucsb.edu",
+    stars: 1,
+    dateReviewed: "2022-01-02T12:00:00",
+    comments: "bad",
+  },
+  threeReviews: [
+    {
       id: 1,
       itemId: "10",
       reviewerEmail: "cgaucho@ucsb.edu",
@@ -7,33 +16,23 @@ const menuItemReviewFixtures = {
       dateReviewed: "2022-01-02T12:00:00",
       comments: "bad",
     },
-    threeReviews: [
-      {
-        id: 1,
-        itemId: "10",
-        reviewerEmail: "cgaucho@ucsb.edu",
-        stars: 1,
-        dateReviewed: "2022-01-02T12:00:00",
-        comments: "bad",
-      },
-      {
-        id: 2,
-        itemId: "11",
-        reviewerEmail: "cgaucho1@ucsb.edu",
-        stars: 3,
-        dateReviewed: "2022-01-02T12:00:00",
-        comments: "ok",
-      },
-      {
-        id: 3,
-        itemId: "12",
-        reviewerEmail: "cgaucho2@ucsb.edu",
-        stars: 5,
-        dateReviewed: "2022-01-02T12:00:00",
-        comments: "good",
-      },
-    ],
-  };
-  
-  export { menuItemReviewFixtures };
-  
+    {
+      id: 2,
+      itemId: "11",
+      reviewerEmail: "cgaucho1@ucsb.edu",
+      stars: 3,
+      dateReviewed: "2022-01-02T12:00:00",
+      comments: "ok",
+    },
+    {
+      id: 3,
+      itemId: "12",
+      reviewerEmail: "cgaucho2@ucsb.edu",
+      stars: 5,
+      dateReviewed: "2022-01-02T12:00:00",
+      comments: "good",
+    },
+  ],
+};
+
+export { menuItemReviewFixtures };
