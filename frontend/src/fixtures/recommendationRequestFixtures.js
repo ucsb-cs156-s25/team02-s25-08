b6f37a1be0912a13eaa0a7e955c43df53fbd2784
rbuneti <rbuneti@ucsb.edu>
2025-05-01 18:17:14 -0700

    added recommendationRequestFixtures.js in frontend/src/main/fixtures with proper sample values from the website.

--- frontend/src/fixtures/recommendationRequestFixtures.js
+++ frontend/src/fixtures/recommendationRequestFixtures.js
@@ -1,51 +1,51 @@
 const recommendationRequestsFixtures = {
-    oneRequest: {
+  oneRequest: {
+    id: 1,
+    requesterEmail: "cgaucho@ucsb.edu",
+    professorEmail: "phtcon@ucsb.edu",
+    explanation: "BS/MS program",
+    dateRequested: "2022-04-20T00:00:00",
+    dateNeeded: "2022-05-01T00:00:00",
+    done: false,
+  },
+  fourRequests: [
+    {
       id: 1,
-      requesterEmail: 'cgaucho@ucsb.edu',
-      professorEmail: 'phtcon@ucsb.edu',
-      explanation: 'BS/MS program',
-      dateRequested: '2022-04-20T00:00:00',
-      dateNeeded: '2022-05-01T00:00:00',
+      requesterEmail: "cgaucho@ucsb.edu",
+      professorEmail: "phtcon@ucsb.edu",
+      explanation: "BS/MS program",
+      dateRequested: "2022-04-20T00:00:00",
+      dateNeeded: "2022-05-01T00:00:00",
       done: false,
     },
-    fourRequests: [
-      {
-        id: 1,
-        requesterEmail: 'cgaucho@ucsb.edu',
-        professorEmail: 'phtcon@ucsb.edu',
-        explanation: 'BS/MS program',
-        dateRequested: '2022-04-20T00:00:00',
-        dateNeeded: '2022-05-01T00:00:00',
-        done: false,
-      },
-      {
-        id: 2,
-        requesterEmail: 'ldelplaya@ucsb.edu',
-        professorEmail: 'richert@ucsb.edu',
-        explanation: 'PhD CS Stanford',
-        dateRequested: '2022-05-20T00:00:00',
-        dateNeeded: '2022-11-15T00:00:00',
-        done: false,
-      },
-      {
-        id: 3,
-        requesterEmail: 'ldelplaya@ucsb.edu',
-        professorEmail: 'phtcon@ucsb.edu',
-        explanation: 'PhD CS Stanford',
-        dateRequested: '2022-05-20T00:00:00',
-        dateNeeded: '2022-11-15T00:00:00',
-        done: false,
-      },
-      {
-        id: 4,
-        requesterEmail: 'alu@ucsb.edu',
-        professorEmail: 'phtcon@ucsb.edu',
-        explanation: 'PhD CE Cal Tech',
-        dateRequested: '2022-05-20T00:00:00',
-        dateNeeded: '2022-11-15T00:00:00',
-        done: false,
-      },
-    ],
-  };
-  
-  export { recommendationRequestsFixtures };  
+    {
+      id: 2,
+      requesterEmail: "ldelplaya@ucsb.edu",
+      professorEmail: "richert@ucsb.edu",
+      explanation: "PhD CS Stanford",
+      dateRequested: "2022-05-20T00:00:00",
+      dateNeeded: "2022-11-15T00:00:00",
+      done: false,
+    },
+    {
+      id: 3,
+      requesterEmail: "ldelplaya@ucsb.edu",
+      professorEmail: "phtcon@ucsb.edu",
+      explanation: "PhD CS Stanford",
+      dateRequested: "2022-05-20T00:00:00",
+      dateNeeded: "2022-11-15T00:00:00",
+      done: false,
+    },
+    {
+      id: 4,
+      requesterEmail: "alu@ucsb.edu",
+      professorEmail: "phtcon@ucsb.edu",
+      explanation: "PhD CE Cal Tech",
+      dateRequested: "2022-05-20T00:00:00",
+      dateNeeded: "2022-11-15T00:00:00",
+      done: false,
+    },
+  ],
+};
+
+export { recommendationRequestsFixtures };
