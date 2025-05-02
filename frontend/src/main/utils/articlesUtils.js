8004421e5e9cf401ac13a1ebb84c85f388727c56
Selena Zeng <selenazeng@ucsb.edu>
2025-05-02 03:07:54 -0700

    sz - add Articles table

---
+++ frontend/src/main/utils/articlesUtils.js
@@ -0,0 +1,13 @@
+export function onDeleteSuccess(message) {
+    console.log(message);
+}
+
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/articles",
+        method: "DELETE",
+        params: {
+            id: cell.row.values.id
+        }
+    }
+}
