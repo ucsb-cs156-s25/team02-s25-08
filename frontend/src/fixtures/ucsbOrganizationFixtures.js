11158482c0df1f2a251f2c4eb64be281a52793c7
hannuhsu <yasu@ucsb.edu>
2025-05-02 11:50:26 -0700

    ran prettier

--- frontend/src/fixtures/ucsbOrganizationFixtures.js
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -1,35 +1,35 @@
 const organizationFixtures = {
-    oneOrganization: [
-      {
-        orgCode: "SKY",
-        orgTranslationShort: "SKYDIVING CLUB",
-        orgTranslation: "SKYDIVING CLUB AT UCSB",
-        inactive: false,
-      },
-    ],
-  
-    threeOrganizations: [
-      {
-        orgCode: "ZPR",
-        orgTranslationShort: "ZETA PHI RHO",
-        orgTranslation: "ZETA PHI RHO",
-        inactive: false,
-      },
-  
-      {
-        orgCode: "KRC",
-        orgTranslationShort: "KOREAN RADIO CL",
-        orgTranslation: "KOREAN RADIO CLUB",
-        inactive: false,
-      },
-  
-      {
-        orgCode: "OSLI",
-        orgTranslationShort: "STUDENT LIFE",
-        orgTranslation: "OFFICE OF STUDENT LIFE",
-        inactive: false,
-      },
-    ],
-  };
-  
-  export { organizationFixtures };
+  oneOrganization: [
+    {
+      orgCode: "SKY",
+      orgTranslationShort: "SKYDIVING CLUB",
+      orgTranslation: "SKYDIVING CLUB AT UCSB",
+      inactive: false,
+    },
+  ],
+
+  threeOrganizations: [
+    {
+      orgCode: "ZPR",
+      orgTranslationShort: "ZETA PHI RHO",
+      orgTranslation: "ZETA PHI RHO",
+      inactive: false,
+    },
+
+    {
+      orgCode: "KRC",
+      orgTranslationShort: "KOREAN RADIO CL",
+      orgTranslation: "KOREAN RADIO CLUB",
+      inactive: false,
+    },
+
+    {
+      orgCode: "OSLI",
+      orgTranslationShort: "STUDENT LIFE",
+      orgTranslation: "OFFICE OF STUDENT LIFE",
+      inactive: false,
+    },
+  ],
+};
+
+export { organizationFixtures };
