779ae441f96df602c36eaf0805d6da5f1cf83e91
hannuhsu <yasu@ucsb.edu>
2025-05-02 21:21:30 -0700

    placeholders fix

--- frontend/src/main/pages/UCSBOrganizations/UCSBOrganizationsCreatePage.js
+++ frontend/src/main/pages/UCSBOrganizations/UCSBOrganizationsCreatePage.js
@@ -1,11 +1,46 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import UCSBOrganizationsForm from "main/components/UCSBOrganizations/UCSBOrganizationsForm";
+import { Navigate } from "react-router-dom";
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function UCSBOrganizationsCreatePage() {
-  // Stryker disable all : placeholder for future implementation
+export default function UCSBOrganizationsCreatePage({ storybook = false }) {
+  const objectToAxiosParams = (ucsborganization) => ({
+    url: "/api/ucsborganizations/post",
+    method: "POST",
+    params: {
+      orgCode: ucsborganization.orgCode,
+      orgTranslationShort: ucsborganization.orgTranslationShort,
+      orgTranslation: ucsborganization.orgTranslation,
+      inactive: ucsborganization.inactive,
+    },
+  });
+
+  const onSuccess = (ucsborganization) => {
+    toast(
+      `New organization Created - id: ${ucsborganization.id} orgCode: ${ucsborganization.orgCode}`,
+    );
+  };
+
+  const mutation = useBackendMutation(objectToAxiosParams, { onSuccess }, [
+    "/api/ucsborganization/all",
+  ]);
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/ucsborganizations" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New UCSB Organization</h1>
+        <UCSBOrganizationsForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   );
