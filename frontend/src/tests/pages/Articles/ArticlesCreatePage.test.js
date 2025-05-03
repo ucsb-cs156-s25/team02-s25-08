cd243e08cc373768623de146a770025804ac0676
Selena Zeng <selenazeng@ucsb.edu>
2025-05-02 18:25:35 -0700

    create page

--- frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
+++ frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
 import ArticlesCreatePage from "main/pages/Articles/ArticlesCreatePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
@@ -8,10 +8,34 @@ import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
+const mockToast = jest.fn();
+jest.mock("react-toastify", () => {
+  const originalModule = jest.requireActual("react-toastify");
+  return {
+    __esModule: true,
+    ...originalModule,
+    toast: (x) => mockToast(x),
+  };
+});
+
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => {
+  const originalModule = jest.requireActual("react-router-dom");
+  return {
+    __esModule: true,
+    ...originalModule,
+    Navigate: (x) => {
+      mockNavigate(x);
+      return null;
+    },
+  };
+});
+
 describe("ArticlesCreatePage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
 
-  const setupUserOnly = () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
     axiosMock.reset();
     axiosMock.resetHistory();
     axiosMock
@@ -20,15 +44,10 @@ describe("ArticlesCreatePage tests", () => {
     axiosMock
       .onGet("/api/systemInfo")
       .reply(200, systemInfoFixtures.showingNeither);
-  };
+  });
 
   const queryClient = new QueryClient();
-  test("Renders expected content", async () => {
-    // arrange
-
-    setupUserOnly();
-
-    // act
+  test("renders without crashing", async () => {
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
@@ -37,8 +56,66 @@ describe("ArticlesCreatePage tests", () => {
       </QueryClientProvider>,
     );
 
-    // assert
+    await waitFor(() => {
+      expect(screen.getByTestId("ArticlesForm-title")).toBeInTheDocument();
+    });
+  });
 
-    await screen.findByText("Create page not yet implemented");
+  test("on submit, makes request to backend, and redirects to /articles", async () => {
+    const queryClient = new QueryClient();
+    const article = {
+      id: 3,
+      title: "New CS Article",
+      url: "https://www.cs.ucsb.edu/article1",
+      explanation: "New research in Computer Science",
+      email: "researcher@ucsb.edu",
+      dateAdded: "2024-02-01T12:00"
+    };
+
+    axiosMock.onPost("/api/articles/post").reply(202, article);
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <ArticlesCreatePage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      expect(screen.getByTestId("ArticlesForm-title")).toBeInTheDocument();
+    });
+
+    const titleInput = screen.getByTestId("ArticlesForm-title");
+    const urlInput = screen.getByTestId("ArticlesForm-url");
+    const explanationInput = screen.getByTestId("ArticlesForm-explanation");
+    const emailInput = screen.getByTestId("ArticlesForm-email");
+    const dateAddedInput = screen.getByTestId("ArticlesForm-dateAdded");
+    const submitButton = screen.getByTestId("ArticlesForm-submit");
+
+    fireEvent.change(titleInput, { target: { value: "New CS Article" } });
+    fireEvent.change(urlInput, { target: { value: "https://www.cs.ucsb.edu/article1" } });
+    fireEvent.change(explanationInput, { target: { value: "New research in Computer Science" } });
+    fireEvent.change(emailInput, { target: { value: "researcher@ucsb.edu" } });
+    fireEvent.change(dateAddedInput, { target: { value: "2024-02-01T12:00" } });
+    
+    expect(submitButton).toBeInTheDocument();
+
+    fireEvent.click(submitButton);
+
+    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+    expect(axiosMock.history.post[0].params).toEqual({
+      title: "New CS Article",
+      url: "https://www.cs.ucsb.edu/article1",
+      explanation: "New research in Computer Science",
+      email: "researcher@ucsb.edu",
+      dateAdded: "2024-02-01T12:00"
+    });
+
+    expect(mockToast).toBeCalledWith(
+      "New article Created - id: 3 title: New CS Article"
+    );
+    expect(mockNavigate).toBeCalledWith({ to: "/articles" });
   });
 });
