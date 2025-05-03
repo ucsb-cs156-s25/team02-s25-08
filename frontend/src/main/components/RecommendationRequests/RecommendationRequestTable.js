2436b1024b8169b535044e449c2b9fbe2b0b1147
rbuneti <rbuneti@ucsb.edu>
2025-05-03 16:50:26 -0700

    rb- added table component for recommendationrequest with tests

---
+++ frontend/src/main/components/RecommendationRequests/RecommendationRequestTable.js
@@ -0,0 +1,69 @@
+// frontend/src/main/components/RecommendationRequests/RecommendationRequestTable.js
+
+import React from "react";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import {
+  cellToAxiosParamsDelete,
+  onDeleteSuccess,
+} from "main/utils/RecommendationRequestUtils";
+import { useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
+
+export default function RecommendationRequestTable({ requests, currentUser }) {
+  const navigate = useNavigate();
+
+  const editCallback = (cell) => {
+    navigate(`/recommendationrequests/edit/${cell.row.values.id}`);
+  };
+
+  // Stryker disable all : hard to test for query caching
+  const deleteMutation = useBackendMutation(
+    cellToAxiosParamsDelete,
+    { onSuccess: onDeleteSuccess },
+    ["/api/recommendationrequests/all"],
+  );
+  // Stryker restore all
+
+  // Stryker disable next-line all : TODO test delete
+  const deleteCallback = async (cell) => {
+    deleteMutation.mutate(cell);
+  };
+
+  const columns = [
+    { Header: "ID", accessor: "id" },
+    { Header: "Requester Email", accessor: "requesterEmail" },
+    { Header: "Professor Email", accessor: "professorEmail" },
+    { Header: "Explanation", accessor: "explanation" },
+    { Header: "Date Requested", accessor: "dateRequested" },
+    { Header: "Date Needed", accessor: "dateNeeded" },
+    { Header: "Done?", accessor: "done" },
+  ];
+
+  if (hasRole(currentUser, "ROLE_ADMIN")) {
+    columns.push(
+      ButtonColumn(
+        "Edit",
+        "primary",
+        editCallback,
+        "RecommendationRequestTable",
+      ),
+    );
+    columns.push(
+      ButtonColumn(
+        "Delete",
+        "danger",
+        deleteCallback,
+        "RecommendationRequestTable",
+      ),
+    );
+  }
+
+  return (
+    <OurTable
+      data={requests}
+      columns={columns}
+      testid={"RecommendationRequestTable"}
+    />
+  );
+}
---
+++ frontend/src/stories/components/RecommendationRequests/RecommendationRequestTable.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+import RecommendationRequestTable from "main/components/RecommendationRequests/RecommendationRequestTable";
+import { recommendationRequestFixtures } from "fixtures/recommendationRequestFixtures";
+import { currentUserFixtures } from "fixtures/currentUserFixtures";
+import { http, HttpResponse } from "msw";
+
+export default {
+  title: "components/RecommendationRequests/RecommendationRequestTable",
+  component: RecommendationRequestTable,
+};
+
+const Template = (args) => {
+  return <RecommendationRequestTable {...args} />;
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  requests: [],
+  currentUser: currentUserFixtures.userOnly,
+};
+
+export const ThreeItemsOrdinaryUser = Template.bind({});
+ThreeItemsOrdinaryUser.args = {
+  requests: recommendationRequestFixtures.threeRequests,
+  currentUser: currentUserFixtures.userOnly,
+};
+
+export const ThreeItemsAdminUser = Template.bind({});
+ThreeItemsAdminUser.args = {
+  requests: recommendationRequestFixtures.threeRequests,
+  currentUser: currentUserFixtures.adminUser,
+};
+ThreeItemsAdminUser.parameters = {
+  msw: [
+    http.delete("/api/recommendationrequests", () => {
+      return HttpResponse.json({}, { status: 200 });
+    }),
+  ],
+};
