2966c3a65adb3e24b6c4f5f06fe1c4564c015a97
hannuhsu <yasu@ucsb.edu>
2025-05-02 21:33:21 -0700

    prettier

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -75,12 +75,12 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/placeholder">
                     Placeholder
                   </Nav.Link>
-               <Nav.Link as={Link} to="/ucsborganizations">
-                 UCSBOrganizations
-               </Nav.Link>
-               <Nav.Link as={Link} to="/helprequest">
-                 Help Requests
-                   </Nav.Link>
+                  <Nav.Link as={Link} to="/ucsborganizations">
+                    UCSBOrganizations
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/helprequest">
+                    Help Requests
+                  </Nav.Link>
                 </>
               ) : (
                 <></>
