4989656d34829a572b17539d141f2936fd014b5f
giozucca <80614385+giozucca@users.noreply.github.com>
2025-05-08 13:33:42 -0700

    restore stryker disable all to file; fix api endpoint url

--- frontend/src/main/components/UCSBOrganizations/UCSBOrganizationsTable.js
+++ frontend/src/main/components/UCSBOrganizations/UCSBOrganizationsTable.js
@@ -20,15 +20,19 @@ export default function UCSBOrganizationsTable({
     navigate(`/UCSBOrganizations/edit/${cell.row.values.id}`);
   };
 
+  // Stryker disable all : hard to test for query caching
   const deleteMutation = useBackendMutation(
     cellToAxiosParamsDelete,
     { onSuccess: onDeleteSuccess },
-    ["/api/ucsborganizations/all"],
+    ["/api/ucsborganization/all"],
   );
+  // Stryker restore all
 
+  // Stryker disable next-line all : TODO try to make a good test for this
   const deleteCallback = async (cell) => {
     deleteMutation.mutate(cell);
   };
+  
 
   const columns = [
     {
