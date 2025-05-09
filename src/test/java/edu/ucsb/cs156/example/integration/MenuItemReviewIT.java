f04c4527fddfdc6ba8845c1108052b5ff55505ed
Tiffany Gu <tiffanygu@ucsb.edu>
2025-05-09 13:59:26 -0700

    tg-added missing date import which prevented maven compilation

--- src/test/java/edu/ucsb/cs156/example/integration/MenuItemReviewIT.java
+++ src/test/java/edu/ucsb/cs156/example/integration/MenuItemReviewIT.java
@@ -29,6 +29,7 @@ import edu.ucsb.cs156.example.repositories.UserRepository;
 import edu.ucsb.cs156.example.services.CurrentUserService;
 import edu.ucsb.cs156.example.services.GrantedAuthoritiesService;
 import edu.ucsb.cs156.example.testconfig.TestConfig;
+import java.time.LocalDateTime;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
